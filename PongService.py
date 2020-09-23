e2ba2d9a8685d3aae6baa5b32cda662080bdd9c8
cordovaom <cordovaom@mymail.vcu.edu>
2020-09-23 02:11:55 -0400

    created files
    created pong server and run.sh to run the server

---
+++ PongService.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'secret key here'
+    
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify({'message': 'Page Not Found'}), 404
+    
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify({'message': 'Servier Error'}), 500
+
+@app.route('/user', methods=['GET'])
+def pong():
+    return jsonify({'message': 'Server has ponged'}), 200
---
+++ run.sh
@@ -0,0 +1,2 @@
+export FLASK_APP=PongService.py
+flask run
